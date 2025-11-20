338daa20ce6b5cc570abf727b01b76c849fdeb0c
Ana Martins <ana.costa.martins@rtl.nl>
2025-11-20 15:06:58 +0100

    changes in test file

--- src/playwright/tests/tracker.spec.ts
+++ src/playwright/tests/tracker.spec.ts
@@ -7,12 +7,18 @@ test('Tracker', async ({ page }) => {
 	await page.goto(hrSelfServiceUrl);
 	await page.waitForURL(hrSelfServiceLoggedInUrl, { timeout: 120000 });
 	await page.locator('#Navigatie').contentFrame().getByTestId('Start').click();
+	// await page.waitForTimeout(10000);
 	await page.locator('iframe[name="Basis"]').contentFrame().getByRole('link', { name: /Mijn declaraties/ }).click();
+	// await page.waitForTimeout(10000);
 	await page.locator('iframe[name="Basis"]').contentFrame().locator('#tab_6').getByText(/Declaratie woon-werk/).click();
+	// await page.waitForTimeout(10000);
 
 	await page.locator('iframe[name="Basis"]').contentFrame().locator('frame[name="Details"]').contentFrame().locator('[id="ctl00_ContentPlaceHolder_uiSpreadControl_uiSpreadControl_FpSpread_5,3"]').selectOption('November');
+	await page.waitForTimeout(10000);
 	await page.locator('iframe[name="Basis"]').contentFrame().locator('frame[name="Details"]').contentFrame().locator('[id="ctl00_ContentPlaceHolder_uiSpreadControl_uiSpreadControl_FpSpread_10,3"]').selectOption('OV kosten');
 	await page.locator('iframe[name="Basis"]').contentFrame().locator('frame[name="Details"]').contentFrame().locator('[id="ctl00_ContentPlaceHolder_uiSpreadControl_uiSpreadControl_FpSpread_11,3"]').selectOption('OV kosten');
+	await page.locator('iframe[name="Basis"]').contentFrame().locator('frame[name="Details"]').contentFrame().locator('[id="ctl00_ContentPlaceHolder_uiSpreadControl_uiSpreadControl_FpSpread_12,3"]').selectOption('OV kosten');
+	await page.locator('iframe[name="Basis"]').contentFrame().locator('frame[name="Details"]').contentFrame().locator('[id="ctl00_ContentPlaceHolder_uiSpreadControl_uiSpreadControl_FpSpread_13,3"]').selectOption('OV kosten');
   	await page.locator('iframe[name="Basis"]').contentFrame().locator('frame[name="Details"]').contentFrame().locator('.s41s22').dblclick();
   	await page.locator('iframe[name="Basis"]').contentFrame().locator('frame[name="Details"]').contentFrame().locator('#ctl00_ContentPlaceHolder_uiSpreadControl_uiSpreadControl_FpSpread_ctl32').fill('100.00');
   	// await page.locator('iframe[name="Basis"]').contentFrame().locator('frame[name="Details"]').contentFrame().locator('tr:nth-child(40) > td:nth-child(7)').click();
